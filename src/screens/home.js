605246934817270696cbc2fbfe8e15033e598b38
emiliocramer <emilio.cramer101@gmail.com>
2024-04-19 08:01:27 -0700

    add pizzicato functionality

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,32 +1,38 @@
 import { useState, useEffect } from 'react';
 import './home.css';
 import { Slider } from "../components/slider";
-import { useGlobalAudioPlayer } from 'react-use-audio-player';
+import Pizzicato from "pizzicato/distr/Pizzicato";
 
 export const HomeScreen = () => {
     const [currentSection, setCurrentSection] = useState(-1);
     const [isPlaying, setIsPlaying] = useState(false);
-    const [hasPlayed, setHasPlayed] = useState(false);
-    const { load, setRate, play, pause } = useGlobalAudioPlayer();
+    const [loadedSound, setLoadedSound] = useState(null);
+
+    useEffect(() => {
+        const sound = new Pizzicato.Sound( require('../assets/blinding-lights.mp3'), () => {
+            console.log('Sound file loaded!');
+        });
+
+        setLoadedSound(sound);
+
+        return () => {
+            sound.stop();
+            sound.disconnect();
+        };
+    }, []);
 
     const handlePlay = () => {
-        if (!isPlaying && !hasPlayed) {
-            load(require('../assets/blinding-lights.mp3'), {
-                autoplay: true,
-                html5: true,
-                rate: 1,
-                format: 'mp3'
-            });
-            setIsPlaying(true);
-            setHasPlayed(true)
-        } else if (!isPlaying && hasPlayed) {
-            play();
-            setIsPlaying(true);
+        if (loadedSound) {
+            if (!isPlaying) {
+                loadedSound.play();
+            } else {
+                loadedSound.pause();
+            }
+            setIsPlaying(!isPlaying);
         } else {
-            pause();
-            setIsPlaying(false);
+            console.error('Sound not loaded');
         }
-    }
+    };
 
     useEffect(() => {
         const handleMouseMove = (event) => {
@@ -38,7 +44,6 @@ export const HomeScreen = () => {
 
             if (sectionIndex !== currentSection) {
                 setCurrentSection(sectionIndex);
-                console.log(`Mouse is over section ${sectionIndex} (${sectionIndex * 5}% - ${(sectionIndex + 1) * 5}%)`);
             }
         };
 
@@ -49,8 +54,11 @@ export const HomeScreen = () => {
     }, [currentSection]);
 
     useEffect(() => {
-        setRate(0.5 + ((currentSection - 1) / (20 - 1)) * (2 - 0.5))
-    }, [setRate, currentSection])
+        if (loadedSound) {
+            loadedSound.playbackRate = 0.5 + ((currentSection - 1) / (20 - 1)) * (2 - 0.5);
+            console.log(loadedSound.playbackRate)
+        }
+    }, [currentSection, loadedSound])
 
     return (
         <div className="home-container">
